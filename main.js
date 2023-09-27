8c8ffa2cb177f1711de3adba0b20809904cd87c9
Sejal-Pradhan <132825887+Sejal-Pradhan@users.noreply.github.com>
2023-09-27 19:55:59 +0530

    Delete main.js

--- main.js
+++
@@ -1,6 +0,0 @@
-function myPlayFunction() {
-    document.getElementById("train").style.animationPlayState = "running";
-}
-function myPauseFunction() {
-    document.getElementById("train").style.animationPlayState = "paused";
-}
