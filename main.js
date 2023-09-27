db2d85dbe975c8c5139ed8df697191f5fefc7d98
Sejal-Pradhan <132825887+Sejal-Pradhan@users.noreply.github.com>
2023-09-27 19:45:03 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+function myPlayFunction() {
+    document.getElementById("train").style.animationPlayState = "running";
+}
+function myPauseFunction() {
+    document.getElementById("train").style.animationPlayState = "paused";
+}
